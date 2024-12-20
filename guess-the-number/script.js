2e4eb6d6515550870344c6b2128de41567b227a8
SzaBee13 <miabajodlol@gmail.com>
2024-12-20 13:05:19 +0100

    guess the number v2
    localstorige, change numbers

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -1,17 +1,26 @@
 const userInput = document.getElementById("guess")
 const scoreSpan = document.getElementById("score")
-const lsScore = localStorage.getItem("gtn-score")
 const secret = document.getElementById("secret")
 const fullscreenDiv = document.getElementById('fullscreen-div');
+const minNumSpan = document.getElementById("min")
+const maxNumSpan = document.getElementById("max")
+
+const lsScore = localStorage.getItem("gtn-score")
+const lsMin = localStorage.getItem("gtn-min")
+const lsMax = localStorage.getItem("gtn-max")
+
 let randomNumber;
+let lPressCount = 0;
+let resetTimeout;
+let score = 0;
+let minNum = 1;
+let maxNum = 100;
 
 function startGame() {
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+    randomNumber = Math.floor(Math.random() * maxNum) + minNum;
     secret.innerHTML = randomNumber
     console.log("game started")
 }
-
-let score = 0
 
 function checkGuess() {
     const userGuess = parseInt(userInput.value);
@@ -39,12 +48,51 @@ function addScore() {
     console.log("score added")
 }
 
+function updateMinMax(sel) {
+    if (sel == 'min') {
+        const value = minNumSpan.value
+        minNum = parseInt(value)
+        localStorage.setItem("gtn-min", String(value))
+    } else if (sel == 'max') {
+        const value = maxNumSpan.value
+        maxNum = parseInt(value)
+        localStorage.setItem("gtn-max", String(value))
+    }
+}
+
 function notShowFullScreen() {
     fullscreenDiv.style.display = 'none'; // Div megjelenítése
 }
 
-let lPressCount = 0;
-let resetTimeout;
+function init() {
+    if (lsScore) {
+        scoreSpan.innerHTML = lsScore;
+        score = parseInt(lsScore);
+    } else {
+        score = 0;
+        localStorage.setItem("gtn-score", "0");
+    }
+
+    if (lsMax) {
+        maxNumSpan.value = parseInt(lsMax);
+        maxNum = parseInt(lsMax);
+    } else {
+        maxNum = 100;
+        localStorage.setItem("gtn-max", String(maxNum));
+        maxNumSpan.innerHTML = String(maxNum);
+    }
+
+    if (lsMin) {
+        minNumSpan.value = parseInt(lsMin);
+        minNum = parseInt(lsMin);
+    } else {
+        minNum = 1;
+        localStorage.setItem("gtn-min", String(minNum));
+        minNumSpan.innerHTML = String(minNum)
+    }
+
+    startGame();
+}
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'l' || event.key === 'L') {
@@ -66,17 +114,5 @@ document.addEventListener('keydown', (event) => {
         }, 2000);
     }
 });
-
-function init() {
-    if (lsScore) {
-        scoreSpan.innerHTML = lsScore
-        score = parseInt(lsScore)
-    } else {
-        score = 0
-        localStorage.setItem("gtn-score", "0")
-    }
-    console.log("init is runned")
-    startGame()
-}
 
 init()
