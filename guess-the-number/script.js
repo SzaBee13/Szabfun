354fc65601fedfbe43f3b53c74f28b69686c9790
SzaBee13 <miabajodlol@gmail.com>
2025-10-11 21:22:10 +0200

    change api url

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -9,7 +9,7 @@ const lsScore = localStorage.getItem("gtn-score");
 const lsMin = localStorage.getItem("gtn-min");
 const lsMax = localStorage.getItem("gtn-max");
 
-const apiUrl = "https://olddell.wampus-enigmatic.ts.net/szabfun";
+const apiUrl = "https://olddell.wampus-enigmatic.ts.net";
 
 let randomNumber;
 let lPressCount = 0;
